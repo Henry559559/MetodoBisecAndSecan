cdf8e578110d5d7ca4eace2868e38f4fac723c8e
Henry559559 <63005043+Henry559559@users.noreply.github.com>
2022-11-29 22:37:11 -0500

    Add files via upload

--- SecanteTest.java
+++ SecanteTest.java
@@ -4,14 +4,16 @@ public class SecanteTest {
 
     public static void main(String[] args) {
         /**
-         * aqui Se ejecuta el programa se pone la funcion o el metodo
-         * Que se va a secante Recordatoria antes de ejecutar priemro se debe escribir mano la funcion a secante
-         * y se debe bajar los jars ext-1.1.1
-         * y  jep-2.4.1 o la libreria Jep  de hai desconmprirmir el archivo ir se a la carpeta darle doble clip y buscar
-         * la carpeta que diga dist y ahi estan los jars a agregar a la libreria del programa
+         *aquí Se ejecuta el programa se pone la función o el método 
+         * que se va a secante Recordatoria antes de ejecutar, 
+         * 1) primero se debe escribir mano la función a secante, 
+         * se debe bajar los jars (ext-1.1.1 y jep-2.4.1) o la 
+         * librería Jep  de ahí descomprimir el archivo ir se a la 
+         * carpeta darle doble clip y buscar la carpeta que diga dist y 
+         * ahí están los jars a agregar a la librería del programa.
          * */
         Funcion f = new Funcion("x^2-6.8*x^1.01+9");
         Secante s = new Secante();
-        System.out.println(s.raiz(f, 0, 1000, 1000, 1e-8));
+        System.out.println(s.raiz(f, 5, 6, 10, 1e-6));
     }
 }
